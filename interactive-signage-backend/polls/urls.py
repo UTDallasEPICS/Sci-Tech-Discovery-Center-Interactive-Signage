394c8027a0c3541c533b1a936ab970ca17474b09
aayush <aayush7511@gmail.com>
2025-12-03 16:06:44 -0600

    button and scanner updated

--- interactive-signage-backend/polls/urls.py
+++ interactive-signage-backend/polls/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("showinfo/", views.showinfo, name="showinfo"),
     path("receive-id/", views.receive_den_id, name="receive_den_id"),
     path("resetinfo/", views.restartflag, name="restartflag"),
+    path("receive-button/", views.receive_button, name="receive_button"),
 ]
